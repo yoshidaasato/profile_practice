b828507c50c44e09990cb0643a8a3a1948e81ad9
yoshidaasato <akkunn1026@gmail.com>
2025-03-04 01:54:52 +0900

    create_self-introduction

---
+++ src/app/profile2/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import { Button , Container } from "@mui/material";
+import Link from 'next/link';
+
+
+function page() {
+  return (
+    <div>
+      <Container>
+        名前：吉田和誠
+        <br />
+        ふりがな：よしだあさと
+        <br />
+        学年：2年
+        <br />
+        学部：情報科学部
+        <br />
+        学科：情報工学科
+        <br />
+        ひとこと：よろしくお願いします。
+        <br />
+        <Link href="https://github.com/yoshidaasato" passHref>
+            <Button variant="contained" color="primary">
+                gitリポジトリはこちらから
+            </Button>
+        </Link>
+        <Link href="/" passHref>
+            <Button variant="contained" color="secondary">
+                戻る
+            </Button>
+        </Link> 
+      </Container>
+    </div>
+
+  );
+}
+
+export default page;
